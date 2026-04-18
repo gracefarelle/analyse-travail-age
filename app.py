2dfe3783c59716bbafc136a8f24716423a3e7ada
gracefarelle <farelle.tchatchouang@facsciences-uy1.cm>
2026-04-18 23:57:35 +0100

    correction indentation ligne 80

--- app.py
+++ app.py
@@ -76,5 +76,5 @@ if __name__ == '__main__':
     db.init_db()
     app.run(debug=True)
 
-    if _name_ == "_main_":
-    app.run(host='0.0.0.0', port=10000)
+    if __name__ == "_main_":
+       app.run(host='0.0.0.0', port=10000)
