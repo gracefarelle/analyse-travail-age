e7079a732c9b3e3993ed56d3fa16b58332958b7b
gracefarelle <farelle.tchatchouang@facsciences-uy1.cm>
2026-04-25 14:05:15 +0100

    mise au propre

--- app.py
+++ app.py
@@ -77,4 +77,4 @@ if __name__ == '__main__':
     db.init_db()
     app.run(debug=True)
 
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 10000)))
+    app.run(host='0.0.0.0',  port=int(os.environ.get("PORT", 10000)))
